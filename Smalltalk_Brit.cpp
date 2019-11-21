e0be8dc55e05a0b9587889eec146e9790d1349fb
Guest <caleb.thompson.17@cnu.edu>
2019-11-20 21:44:33 -0500

    Finished Smalltalk_Brit.cpp. I think?

--- Smalltalk_Brit.cpp
+++ Smalltalk_Brit.cpp
@@ -5,6 +5,20 @@
  *      Author: guest-lq7o0x
  */
 
+#include "includes/Smalltalk_Brit.h"
+#include "includes/constants.h"
 
+//create with appropriate phrases
+Smalltalk_Brit::Smalltalk_Brit(int iPerson /*=1*/) : Smalltalk(BRIT, iPerson) {}
 
+Smalltalk_Brit::~Smalltalk_Brit(void) {}
 
+void Smalltalk_Brit::populatePhrases(){
+	mySmallTalk.push_back(BRIT_1);
+	mySmallTalk.push_back(BRIT_2);
+	mySmallTalk.push_back(BRIT_3);
+	mySmallTalk.push_back(BRIT_4);
+	mySmallTalk.push_back(BRIT_5);
+	mySmallTalk.push_back(BRIT_6);
+	mySmallTalk.push_back(BRIT_7);
+}
